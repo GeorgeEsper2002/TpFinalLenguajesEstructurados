90303bcc56e36e2d6d3bffa33749295bc1970090
Rachid Esper <rachidesper290@gmail.com>
2024-06-04 19:17:53 -0300

    arranque a hacer la estructura del tp

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+struct pregunta{
+    char pregunta;
+    char opt1[30];
+    char opt2[30];
+    char opt3[30];
+    char opt4[30];
+    int correcta;
+};
 int main() {
     printf("Hello, World!\n");
-    printf("Vos podes rachid");
+    char saludo[20];
+    //scanf(" %s",&saludo); no permite ingresar palabras separadas por espacios en blanco
+    gets(saludo);
+    puts(saludo);
+    //strcat(saludo,);
     return 0;
 }
