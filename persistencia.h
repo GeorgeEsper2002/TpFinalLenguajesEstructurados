979aa61e76279a58fdfbf2573a25c2129a462573
unknown <rachidesper290@gmail.com>
2024-06-18 02:15:38 -0300

    Agregue nuevas funciones de busqueda

--- persistencia.h
+++ persistencia.h
@@ -10,10 +10,13 @@ void guardarPreguntaEnArchivo(Pregunta nuevaPregunta);
 void guardarPreguntasEnArchivo(Pregunta preguntas[MAX_PREGUNTAS]);
 void leerPreguntasEnArchivo(Pregunta preguntas[MAX_PREGUNTAS]);
 void guardarPreguntaModificada(Pregunta preguntaModificada);
+void subMenuEliminarPregunta();
 void eliminarPregunta(Pregunta preguntaABorrar);
 int verSiExistePregunta(int idPregunta);
 int verSiExisteCapitulo(int idCapitulo);
-int verSiExisteSubCapitulo(int idSubCapitulo);
+int verSiExisteSubCapitulo(int idCapitulo,int idSubCapitulo);
+Capitulo buscarCapituloPorId(int idCapitulo);
+SubCapitulo buscarSubCapituloPorId(int idCapitulo,int idSubCapitulo);
 
 
 
